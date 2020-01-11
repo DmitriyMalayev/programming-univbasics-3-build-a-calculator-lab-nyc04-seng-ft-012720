61b07dec0555f1225ca5d34af25c6c8b0af83e25
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-11 01:49:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,18 +5,27 @@ puts addition_sum
 end
 
 def subtraction(num1, num2)
+subtraction_sum = num1 - num2
+puts subtraction_sum 
 
 end
 
+
 def division(num1, num2)
+division_sum = num1 / num2
+puts division_sum 
 
 end
 
 def multiplication(num1, num2)
+multiplication_sum = num1 + num2
+puts multiplication_sum
 
 end
 
 def modulo(num1, num2)
+modulo_sum = num1 + num2
+puts modulo_sum 
 
 end
 
